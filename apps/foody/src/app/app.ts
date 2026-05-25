4fb991f3d86121b334d8f5f4ddeb54f04e9ea254
Simon Oczadly <staxx@hotmail.de>
2026-05-25 19:07:49 +0200

    pocketbase first test

--- apps/foody/src/app/app.ts
+++ apps/foody/src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import PocketBase from 'pocketbase';
 
 @Component({
   imports: [NxWelcome, RouterModule],
   selector: 'app-root',
-  templateUrl: './app.html',
+  template: `
+    <h1>Hello World</h1>
+`,
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'foody';
+  pb = new PocketBase('http://127.0.0.1:8090');
+
+  constructor() {
+    this.load();
+  }
+
+  async load() {
+    await this.pb.collection('users').authWithPassword('staxx@hotmail.de', '2}XERs93~cXiHvW'); // Just a local test user
+    const res = await this.pb.collection('TestCollection').getOne('np0fte0uq7pvs8m');
+    console.log(res);
+  }
 }
